217099d913f786dfcc2c49640a0e1e4ca3996309
pranithaiwiz <pranithkumar.8802@gmail.com>
2024-12-16 18:36:19 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{html,js,jsx,ts,tsx}', // All components/pages
-    './public/index.html',            // Static HTML files
-    './components/**/*.{html,js,jsx}' // Additional folders
+    './src/**/*.{html,js,jsx,ts,tsx}', // All source files
+    './public/**/*.html',             // Public HTML files
+    './components/**/*.{html,js,jsx}', // Components directory
   ],
   theme: {
     extend: {},
